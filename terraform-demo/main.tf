f0a2d90724c1fb664f0475e7cdd7af46ccd977dd
Mustafa <117758982+hussainali99a@users.noreply.github.com>
2026-01-18 04:56:03 +0000

    terraform demo completed creating gcp bucket

---
+++ terraform-demo/main.tf
@@ -0,0 +1,31 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "7.16.0"
+    }
+  }
+}
+
+provider "google" {
+  # Configuration options
+  credentials = "/workspaces/data-engineering-zoocamp/terraform-demo/keys/my-creds.json"
+  project = "single-loop-463705-t0"
+  region  = "us-central1"
+}
+
+resource "google_storage_bucket" "demo-bucket" {
+  name          = "data-eng-zoocamp-demo-bucket-terra01"
+  location      = "US"
+  force_destroy = true
+
+
+  lifecycle_rule {
+    condition {
+      age = 1
+    }
+    action {
+      type = "AbortIncompleteMultipartUpload"
+    }
+  }
+}
